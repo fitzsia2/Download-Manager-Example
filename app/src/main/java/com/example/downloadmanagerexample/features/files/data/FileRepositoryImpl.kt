b00598df38698964f122b7b2032a9e3959da79ab
Andrew Fitzsimons <andrew.fitzsimons@ackee.cz>
2023-06-22 11:20:59 +0200

    ⚡ Optimize flow a little

--- app/src/main/java/com/example/downloadmanagerexample/features/files/data/FileRepositoryImpl.kt
+++ app/src/main/java/com/example/downloadmanagerexample/features/files/data/FileRepositoryImpl.kt
@@ -5,11 +5,10 @@ import com.example.downloadmanagerexample.core.data.RemoteFileDataSource
 import com.example.downloadmanagerexample.features.files.domain.CachedFileState
 import com.example.downloadmanagerexample.features.files.domain.FileRepository
 import com.example.downloadmanagerexample.features.files.domain.RemoteFileMetadata
-import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.isActive
 import kotlin.time.Duration.Companion.milliseconds
 
 class FileRepositoryImpl(
@@ -27,12 +26,12 @@ class FileRepositoryImpl(
 
     override fun getCachedFileStateStream(metadata: List<RemoteFileMetadata>): Flow<List<CachedFileState>> {
         return flow {
-            while (currentCoroutineContext().isActive) {
+            while (true) {
                 val cacheStates = metadata.map { getCacheState(it) }
                 emit(cacheStates)
                 delay(DOWNLOAD_STATE_POLL_PERIOD)
             }
-        }
+        }.distinctUntilChanged()
     }
 
     private suspend fun getCacheState(metadata: RemoteFileMetadata): CachedFileState {
